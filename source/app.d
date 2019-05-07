d92d859de7ed1923329fd8e9c4f8c78356653918
Alexey Mironov <amironov73@gmail.com>
2019-05-07 09:57:05 +0800

    First working code

--- source/app.d
+++ source/app.d
@@ -3,34 +3,41 @@ import irbis;
 
 void testEncodings()
 {
-	writeln(toAnsi("Hello"));
-	writeln(fromAnsi([72, 101, 108, 108, 111]));
-	writeln(toUtf("Hello"));
-	writeln(fromUtf([72, 101, 108, 108, 111]));
-	writeln(toAnsi("Привет!"));
-	writeln(fromAnsi([207, 240, 232, 226, 229, 242, 33]));
-	writeln(toUtf("Привет!"));
-	writeln(fromUtf([208, 159, 209, 128, 208, 184, 208, 178, 208, 181, 209, 130, 33]));
+    writeln(toAnsi("Hello"));
+    writeln(fromAnsi([72, 101, 108, 108, 111]));
+    writeln(toUtf("Hello"));
+    writeln(fromUtf([72, 101, 108, 108, 111]));
+    writeln(toAnsi("Привет!"));
+    writeln(fromAnsi([207, 240, 232, 226, 229, 242, 33]));
+    writeln(toUtf("Привет!"));
+    writeln(fromUtf([208, 159, 209, 128, 208, 184, 208, 178, 208, 181, 209, 130, 33]));
 }
 
 void main()
 {
-	testEncodings();
+    auto client = new Connection();
+    client.username = "librarian";
+    client.password = "secret";
 
-	auto client = new Connection();
-	client.username = "librarian";
-	client.password = "secret";
+    if (!client.connect())
+    {
+        writeln("Can't connect!");
+        return;
+    }
 
-	if (!client.connect())
-	{
-		writeln("Can't connect!");
-		return;
-	}
+    writeln("Server version=", client.serverVersion);
+    writeln("Interval=", client.interval);
 
-	int maxMfn = client.getMaxMfn("IBIS");
-	writeln("Max MFN=", maxMfn);
+    auto maxMfn = client.getMaxMfn("IBIS");
+    writeln("Max MFN=", maxMfn);
 
-	client.noOp();
+    client.noOp();
 
-	client.disconnect();
+    auto serverVersion = client.getServerVersion();
+    writeln("Organization:", serverVersion.organization);
+
+    auto content = client.readTextFile("3.IBIS.WS.OPT");
+    writeln(content);
+
+    client.disconnect();
 }
--- source/irbis.d
+++ source/irbis.d
@@ -1,8 +1,10 @@
 import std.stdio;
+import std.conv;
 import std.encoding: transcode, Windows1251String;
 import std.random: uniform;
 import std.socket;
 import std.string;
+import std.outbuffer;
 
 //==================================================================
 //
@@ -38,6 +40,15 @@ bool sameString(string s1, string s2)
     return icmp(s1, s2) == 0;
 }
 
+string irbisToDos(string text)
+{
+    return replace(text, "\x1F\x1E", "\n");
+}
+
+string[] irbisToLines(string text)
+{
+    return text.split("\x1F\x1E");
+}
 
 //==================================================================
 
@@ -159,25 +170,112 @@ final class TreeFile
 
 //==================================================================
 
+/**
+ * Information about the IRBIS64 server version.
+ */
+final class VersionInfo
+{
+    /**
+     * License owner organization.
+     */
+    string organization;
+
+    /**
+     * Server version itself. Example: 64.2008.1
+     */
+    string serverVersion;
+
+    /**
+     * Maximum simultaneous connected client number.
+     */
+    int maxClients;
+
+    /**
+     * Current connected clients number.
+     */
+    int connectedClients;
+
+    this()
+    {
+        organization = "";
+        serverVersion = "";
+        maxClients = 0;
+        connectedClients = 0;
+    }
+
+    /**
+     * Parse the server answer.
+     */
+    void parse(string[] lines) {
+        if (lines.length == 3)
+        {
+            serverVersion = lines[0];
+            connectedClients = to!int(lines[1]);
+            maxClients = to!int(lines[2]);
+        }
+        else
+        {
+            organization = lines[0];
+            serverVersion = lines[1];
+            connectedClients = to!int(lines[2]);
+            maxClients = to!int(lines[3]);
+        }
+    }
+}
+
+//==================================================================
+
 final class ClientQuery
 {
+    private OutBuffer _buffer;
+
     this(Connection connection, string command)
     {
-        // TODO implement
+        _buffer = new OutBuffer();
+        addAnsi(command).newLine();
+        addAnsi(connection.workstation).newLine();
+        addAnsi(command).newLine();
+        add(connection.clientId).newLine();
+        add(connection.queryId).newLine();
+        addAnsi(connection.password).newLine();
+        addAnsi(connection.username).newLine();
+        newLine();
+        newLine();
+        newLine();
+    }
+
+    ClientQuery add(int value)
+    {
+        auto text = to!string(value);
+        return addUtf(text);
     }
 
     ClientQuery addAnsi(string text)
     {
+        auto bytes = toAnsi(text);
+        _buffer.write(bytes);
         return this;
     }
 
     ClientQuery addUtf(string text)
     {
+        auto bytes = toUtf(text);
+        _buffer.write(bytes);
         return this;
+    }
+
+    ubyte[] encode()
+    {
+        auto bytes = _buffer.toBytes();
+        auto result = new OutBuffer();
+        result.printf("%d\n", bytes.length);
+        result.write(bytes);
+        return result.toBytes();
     }
 
     ClientQuery newLine()
     {
+        _buffer.write(cast(byte)10);
         return this;
     }
 }
@@ -186,6 +284,9 @@ final class ClientQuery
 
 final class ServerResponse
 {
+    private ubyte[] _buffer;
+    private ptrdiff_t _offset;
+
     string command;
     int clientId;
     int queryId;
@@ -194,54 +295,159 @@ final class ServerResponse
     string serverVersion;
     int interval;
 
+    this(ubyte[] buffer)
+    {
+        _buffer = buffer;
+        _offset=0;
+
+        command = readAnsi();
+        clientId = readInteger();
+        queryId = readInteger();
+        answerSize = readInteger();
+        serverVersion = readAnsi();
+        interval = readInteger();
+        readAnsi();
+        readAnsi();
+        readAnsi();
+        readAnsi();
+    }
+
+    @property bool eof()
+    {
+        return _offset >= _buffer.length;
+    }
+
     bool checkReturnCode()
     {
-        return false;
+        if (getReturnCode() < 0)
+        {
+            return false;
+        }
+        return true;
     }
 
     ubyte[] getLine()
     {
-        return new ubyte[0];
+        auto result = new OutBuffer();
+
+        while (_offset < _buffer.length)
+        {
+            auto symbol = _buffer[_offset++];
+            if (symbol == 13)
+            {
+                if (_buffer[_offset] == 10)
+                {
+                    _offset++;
+                }
+                break;
+            }
+            result.write(symbol);
+        }
+
+        return result.toBytes();
     }
 
     int getReturnCode()
     {
-        return 0;
+        returnCode = readInteger();
+        return returnCode;
     }
 
     string readAnsi()
     {
-        return "";
+        return fromAnsi(getLine());
     }
 
     int readInteger()
     {
-        return 0;
+        auto line = readUtf();
+        auto result = 0;
+        if (line.length != 0)
+        {
+            result = to!int(line);
+        }
+        return result;
     }
 
     string[] readRemainingAnsiLines()
     {
-        return new string[0];
+        string[] result = new string[0];
+        while (!eof)
+        {
+            auto line = readAnsi();
+            result ~= line;
+        }
+        return result;
     }
 
     string readRemainingAnsiText()
     {
-        return "";
+        auto chunk = _buffer[_offset..$];
+        return fromAnsi(chunk);
     }
 
     string[] readRemainingUtfLines()
     {
-        return new string[0];
+        string[] result = new string[0];
+        while (!eof)
+        {
+            auto line = readUtf();
+            result ~= line;
+        }
+        return result;
     }
 
     string readRemainingUtfText()
     {
-        return "";
+        auto chunk = _buffer[_offset..$];
+        return fromUtf(chunk);
     }
 
     string readUtf()
     {
-        return "";
+        return fromUtf(getLine());
+    }
+}
+
+//==================================================================
+
+class ClientSocket
+{
+    abstract ServerResponse TalkToServer(ClientQuery query);
+}
+
+//==================================================================
+
+final class Tcp4ClientSocket : ClientSocket
+{
+    private Connection _connection;
+
+    this(Connection connection)
+    {
+        _connection = connection;
+    }
+
+    override ServerResponse TalkToServer(ClientQuery query)
+    {
+        auto socket = new Socket(AddressFamily.INET, SocketType.STREAM);
+        auto address = new InternetAddress(_connection.host, _connection.port);
+        socket.connect(address);
+        scope(exit) socket.close();
+        auto outgoing = query.encode();
+        socket.send(outgoing);
+
+        ptrdiff_t amountRead;
+        auto incoming = new OutBuffer();
+        incoming.reserve(2048);
+        auto buffer = new ubyte[2056];
+        while((amountRead = socket.receive(buffer)) != 0)
+        {
+            incoming.write(buffer[0..amountRead]);
+        }
+
+        auto result = new ServerResponse(incoming.toBytes());
+
+        return result;
     }
 }
 
@@ -249,8 +455,10 @@ final class ServerResponse
 
 final class Connection
 {
+    private bool _connected;
+
     string host;
-    uint port;
+    ushort port;
     string username;
     string password;
     string database;
@@ -259,7 +467,7 @@ final class Connection
     int queryId;
     string serverVersion;
     int interval;
-    bool connected;
+    ClientSocket socket;
 
     this()
     {
@@ -267,6 +475,13 @@ final class Connection
         port = 6666;
         database = "IBIS";
         workstation = "C";
+        socket = new Tcp4ClientSocket(this);
+        _connected = false;
+    }
+
+    @property bool connected() const nothrow
+    {
+        return _connected;
     }
 
     bool connect()
@@ -276,14 +491,19 @@ final class Connection
             return true;
         }
 
-        clientId = 123456;
+        clientId = uniform(100_000, 999_999);
         queryId = 1;
         auto query = new ClientQuery(this, "A");
         query.addAnsi(username).newLine();
         query.addAnsi(password);
         auto response = execute(query);
+        response.getReturnCode();
+        if (response.returnCode < 0)
+        {
+            return false;
+        }
 
-        connected = true;
+        _connected = true;
         serverVersion = response.serverVersion;
         interval = response.interval;
 
@@ -300,16 +520,21 @@ final class Connection
         auto query = new ClientQuery(this, "B");
         query.addAnsi(username);
         execute(query);
-        connected = false;
+        _connected = false;
 
         return true;
     }
 
     ServerResponse execute(ClientQuery query)
     {
-        return new ServerResponse();
+        auto result = socket.TalkToServer(query);
+        queryId++;
+        return result;
     }
 
+    /**
+     * Get maximal MFN for the database.
+     */
     int getMaxMfn(string database)
     {
         if (!connected)
@@ -320,10 +545,33 @@ final class Connection
         auto query = new ClientQuery(this, "O");
         query.addAnsi(database);
         auto response = execute(query);
+        response.checkReturnCode();
 
         return response.returnCode;
     }
 
+    /**
+     * Get the server version.
+     */
+    VersionInfo getServerVersion()
+    {
+        auto result = new VersionInfo();
+
+        if (connected)
+        {
+            auto query = new ClientQuery(this, "1");
+            auto response = execute(query);
+            response.checkReturnCode();
+            auto lines = response.readRemainingAnsiLines();
+            result.parse(lines);
+        }
+
+        return result;
+    }
+
+     /**
+      * Empty operation. Confirms the client is alive.
+      */
     bool noOp()
     {
         if (!connected)
@@ -332,6 +580,152 @@ final class Connection
         }
 
         auto query = new ClientQuery(this, "N");
+        execute(query);
+
+        return true;
+    }
+
+    /**
+     * Read the text file from the server.
+     */
+    string readTextFile(string specification)
+    {
+        if (!connected)
+        {
+            return "";
+        }
+
+        auto query = new ClientQuery(this, "L");
+        query.addAnsi(specification).newLine();
+        auto response = execute(query);
+        auto result = response.readAnsi();
+        result = irbisToDos(result);
+
+        return result;
+    }
+
+    /**
+    * Recreate dictionary for the database.
+    */
+    bool reloadDictionary(string database)
+    {
+        if (!connected)
+        {
+            return false;
+        }
+
+        auto query = new ClientQuery(this, "Y");
+        query.addAnsi(database).newLine();
+        execute(query);
+
+        return true;
+    }
+
+    /**
+     * Recreate master file for the database.
+     */
+    bool reloadMasterFile(string database)
+    {
+        if (!connected)
+        {
+            return false;
+        }
+
+        auto query = new ClientQuery(this, "X");
+        query.addAnsi(database).newLine();
+        execute(query);
+
+        return true;
+    }
+
+    /**
+     * Restarting the server (without losing the connected clients).
+     */
+    bool restartServer()
+    {
+        if (!connected)
+        {
+            return false;
+        }
+
+        auto query = new ClientQuery(this, "+8");
+        execute(query);
+
+        return true;
+    }
+
+    /**
+     * Compose the connection string for current connection.
+     * The connection does not have to be established.
+     */
+    string toConnectionString()
+    {
+        return format
+            (
+                "host=%s;port=%d;username=%s;password=%d;database=%s;arm=%s;",
+                host,
+                port,
+                username,
+                password,
+                database,
+                workstation
+            );
+    }
+
+    /**
+     * Empty the database.
+     */
+    bool truncateDatabase(string database)
+    {
+        if (!connected)
+        {
+            return false;
+        }
+
+        auto query = new ClientQuery(this, "S");
+        query.addAnsi(database).newLine();
+        execute(query);
+
+        return true;
+    }
+
+    /**
+     * Unlock the database.
+     */
+    bool unlockDatabase(string database)
+    {
+        if (!connected)
+        {
+            return false;
+        }
+
+        auto query = new ClientQuery(this, "U");
+        query.addAnsi(database).newLine();
+        execute(query);
+
+        return true;
+    }
+
+    /**
+     * Update server INI file lines for current user.
+     */
+    bool updateIniFile(string[] lines)
+    {
+        if (!connected)
+        {
+            return false;
+        }
+
+        if (lines.length == 0)
+        {
+            return true;
+        }
+
+        auto query = new ClientQuery(this, "8");
+        foreach (line; lines)
+        {
+            query.addAnsi(line).newLine();
+        }
         execute(query);
 
         return true;
